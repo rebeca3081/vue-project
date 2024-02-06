f6275db26f071c91e4c5f37f34d63ee3fa05ad0c
rebeca3081 <hyoeun3081@gmail.com>
2024-02-07 01:12:17 +0900

    vue-js_day04_240206_fin

--- user-client/src/views/UserUpdateView.vue
+++ user-client/src/views/UserUpdateView.vue
@@ -1,7 +1,7 @@
 <!-- UserUpdateView.vue -->
 <template>
   <div class="container">
-    <h1>회원 정보 등록</h1>
+    <h1>회원 정보 수정</h1>
     <div class="row">
       <table class="table">
         <tr>
@@ -33,7 +33,6 @@
           <td>
             <input type="radio" value="M" v-model="userInfo.user_gender">남
             <input type="radio" value="F" v-model="userInfo.user_gender">여
-            <!-- <input type="checkbox" true-value="예" false-value="아니오" v-model="chkVal"> -->
           </td>
         </tr>
         <tr>
@@ -46,7 +45,6 @@
           <th>가입날짜</th>
           <td>
             <input type="date" v-model="userInfo.join_date">
-            <!-- yyyy-MM-dd -->
           </td>
         </tr>
       </table>
@@ -75,22 +73,69 @@ export default {
     }
   },
   created() {
+    // 화면이 그려지는 시점에 데이터가 필요함! -> created()에 사용
+    // 라우터로 넘어온 쿼리의 userId값으로 ajax실행하기
     let searchNo = this.$route.query.userId;
-    console.log(searchNo);
     this.getUserInfoes(searchNo);
   },
   methods: {
+    // 회원정보조회 ajax
     async getUserInfoes(userId) {
       //http://localhost:4000/users/admin
       let newPath = `/api/users/${userId}`;
       let result = await axios.get(newPath)
                               .catch(err => console.log(err));
-      console.log(result)
+      // console.log(result)
       let info = result.data;
       this.userInfo = info;
     },
+    // '저장 클릭 시' 회원정보 수정
     updateInfo() {
-      console.log(this.userInfo);
+      // console.log(this.userInfo); // 콘솔로 넘어가는 정보 확인
+      // 1.유효성 체크가 필요할까? 이미 정보가 있으니까 필요 없지 않을까 싶으면서도 정보누락 방지를 위해서 필요할 것 같기도...?
+      
+      // 2. ajax 실행
+      // 2-1. 데이터 선별
+      let data = this.newSendData();
+      console.log(data)
+      // 2-2. axios를 통해 ajax 실행
+      // PUT http://localhost:4000/users/user02
+      axios.put(`/api/users/${data.param.user_id}`, data)
+           .then(result => {
+            console.log(result)
+            // 3.결과 처리
+            if(result.data.changedRows == 0) {
+              alert(`수정되지 않았습니다.\n메세지를 확인해주세요.${result.data.message}`);
+            } else {
+              alert('정상적으로 수정되었습니다. :)');
+            }
+           })
+           .catch(err => console.log(err));
+      
+
+      },
+      newSendData() {
+        let obj = this.userInfo;
+        let delData = ["user_no", "join_date"]; // 가입날짜 입력시 오류나서 제외시킴
+        let newObj = {};
+        let isTargeted = null;
+
+        for (let field in obj) {
+          isTargeted = false;
+          for (let target of delData) {
+            if (field == target) {
+              isTargeted = true;
+              break;
+            }
+          }
+          if (!isTargeted) {
+            newObj[field] = obj[field];
+          }
+        }
+        let sendData = {
+          "param" : newObj
+        }
+        return sendData;
     }
   }
 }
